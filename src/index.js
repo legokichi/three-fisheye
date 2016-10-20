9304471da124b6610dd728b5aa499b90ab2d7fe0
legokichi <legokichi@gmail.com>
2016-10-20 13:13:45 +0900

    update

--- src/index.js
+++ src/index.js
@@ -5,17 +5,22 @@ const Stats = require("stats.js");
 window.THREE = THREE;
 window.$ = $;
 
-function load_video_texture(url) {
+function load_video(url) {
   const video = document.createElement("video");
   return new Promise((resolve, reject)=>{
     video.src = url;
     video.addEventListener("loadeddata", function listener(){
       video.removeEventListener("loadeddata", listener);
-      video.loop = true;
-      video.play();
-      const {videoWidth, videoHeight} = video;
-      resolve(new THREE.VideoTexture( video ));
+      resolve( video );
     });
+  });
+}
+
+function load_video_texture(url) {
+  return load_video(url).then((video)=>{
+    video.loop = true;
+    video.play();
+    return new THREE.VideoTexture( video );
   });
 }
 
@@ -39,6 +44,39 @@ function load_skybox_texture(urls){
 }
 
 
+function load_clipped_video_canvas_texture(url){
+  return load_video(url).then((video)=>{
+    const cnv = document.createElement("canvas");
+    const ctx = cnv.getContext("2d");
+    const {videoWidth, videoHeight} = video;
+    const min = Math.min(videoWidth, videoHeight);
+    const max = Math.max(videoWidth, videoHeight);
+    for(var i=0; min > Math.pow(2, i); i++); // 2^n の大きさを得る
+    let pow = Math.pow(2, i-1);
+    const [dx, dy, dw, dh] = [0, 0, pow, pow]; // 縮小先の大きさ
+    const [sx, sy, sw, sh] = videoWidth < videoHeight ? [0, (max/2)-(min/2), min, min] // ソースのクリッピング領域
+                                                      : [(max/2)-(min/2), 0, min, min];
+    cnv.width = cnv.height = pow;
+    const tex = new THREE.Texture(cnv);
+    let paused = false;
+    video.addEventListener("playing", (ev)=>{ paused = false; requestAnimationFrame(_draw) });
+    video.addEventListener("pause", (ev)=>{ paused = true; });
+    video.addEventListener("ended", (ev)=>{ paused = true; });
+    function _draw(){
+      cnv.width = cnv.width;
+      ctx.drawImage(video, sx, sy, sw, sh, dx, dy, dw, dh);
+      tex.needsUpdate = true;
+      if(!paused) requestAnimationFrame(_draw);
+    }
+    video.loop = true;
+    video.play();
+    _draw(); // clipping draw loop start
+    return tex;
+  });
+}
+
+
+
 function createSkyboxMesh(skybox_texture){
   const cubeShader = THREE.ShaderLib[ 'cube' ];
   cubeShader.uniforms[ 'tCube' ].value = skybox_texture;
@@ -54,23 +92,24 @@ function createSkyboxMesh(skybox_texture){
 }
 
 function createFisheyeMesh(fisheye_texture){
-  const 球体 = new THREE.SphereGeometry(40, 16, 16, 0, Math.PI);
+  const 球体 = new THREE.SphereGeometry(800, 16, 16, 0, Math.PI);
   const {vertices, faces, faceVertexUvs} = 球体;
   const radius = 球体.boundingSphere.radius;
+  // 半球の正射影をとる
   faces.forEach((face, i)=>{
     const {a, b, c} = face;
     faceVertexUvs[0][i] = [a, b, c].map((id)=>{
       const vect3 = vertices[id];
-      // 正射影
       return new THREE.Vector2(
         (vect3.x+radius)/(2*radius),
         (vect3.y+radius)/(2*radius));
     });
   });
   
-  const 完全な白い球体 = new THREE.Mesh(
-    球体,
-    new THREE.MeshBasicMaterial( { color: 0xFFFFFF, map: fisheye_texture, side: THREE.BackSide } ));
+  const mat = new THREE.MeshBasicMaterial( { color: 0xFFFFFF, map: fisheye_texture, side: THREE.BackSide } );
+  //mat.needsUpdate = true;
+  //console.log(mat.needsUpdate)
+  const 完全な白い球体 = new THREE.Mesh(球体, mat);
   完全な白い球体.rotation.x = Math.PI*3/2; // 北緯側の半球になるように回転
   return 完全な白い球体;
 }
@@ -86,8 +125,8 @@ function main(){
   const renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
   container.appendChild(renderer.domElement);
-
-  const camera = new THREE.PerspectiveCamera( 80, window.innerWidth / window.innerHeight, 1, 10000 );
+  // 画角, アスペクト比、視程近距離、視程遠距離
+  const camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 10000 );
   camera.position.z = 0.01;
   scene.add(camera);
 
@@ -119,17 +158,12 @@ function main(){
   // 素材の遅延ロード
   Promise.all([
     load_skybox_texture('textures/cube/skybox/').then(createSkyboxMesh),
-    load_video_texture("./2016-10-18-120852.webm").then(createFisheyeMesh),
+    //load_video_texture("./2016-10-18-120852.webm").then(createFisheyeMesh),
+    load_clipped_video_canvas_texture("./2016-10-18-120852.webm").then(createFisheyeMesh),
     //load_texture("./2016-10-18-16.29.01.png").then(createFisheyeMesh),
   ]).then(([skybox, 完全な白い球体])=>{
     scene.add(skybox);
     scene.add(完全な白い球体);
-    /*
-    const pointLight = new THREE.PointLight(0xFFFFFF);
-    pointLight.position.x = 10;
-    pointLight.position.y = 50;
-    pointLight.position.z = 130;
-    scene.add(pointLight);*/
   })
   .catch(console.error.bind(console));
 }
